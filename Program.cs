41a36e1942108be851418c9f1dc89d61837c2b1d
andrey574 <vinicius03274@gmail.com>
2025-03-24 08:22:32 -0300

    finaliza o programa

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("\t\tSegue abaixo uma contagem regressiva!");
+Console.Write("\n\nPreparar...");
+Console.ReadKey();
+Console.Write("\nApontar...");
+Console.ReadKey();
+Console.Write("\nFOGO!");
